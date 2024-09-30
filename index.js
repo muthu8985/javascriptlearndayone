77ddb9fb07a8ea6ac4af14c377ca3cfb789b7997
Muthu Kumar <muthukumarv8985@gmail.com>
2024-09-30 13:43:42 +0530

    first commit for js basic learn

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("hi my javascript_learndayone");
+
+
+    document.getElementsByClassName(".tag").innerHTML="The heading of h2element";
+
+// document.getElementsByTagName(p).innerHTML="Tried my first paragraph";
+document.getElementById('tag').innerHTML = "React"
+
+// document.getElementsByClassName('tag')[0].innerHTML = 'asdfas'
